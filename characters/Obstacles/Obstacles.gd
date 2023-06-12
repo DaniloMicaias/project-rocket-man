b148d99e875f6d56a600a9e22f6596d6d490096c
Danilo Micaías Lima da Silva <danilo.micaias.sga@gmail.com>
2023-06-12 17:56:35 -0300

    Add Collision

--- characters/Obstacles/Obstacles.gd
+++ characters/Obstacles/Obstacles.gd
@@ -1,8 +1,18 @@
 extends Area2D
 
-
 export (int) var speed = 100
+
+var hp = 1
 
 func _physics_process(delta):
 	global_position.y += (speed-50) * delta
 	global_position.x += speed * delta
+
+func take_damage(damage):
+	hp -= damage
+	if hp <= 0:
+		queue_free()
+
+func _on_Obstacles_area_entered(area):
+	if area is Player:
+		area.take_damage(1)
